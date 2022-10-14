ee4f03f4630dbf30afe3d10a91369b62ff18872c
Michael King'oo <bmuthama.bm2@gmail.com>
2022-10-14 19:22:30 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -8,6 +8,6 @@
  */
 int void(main)
 {
-    printf("\"with proper grammar, but the outcome is a piece of art,");
+    printf("with proper grammar, but the outcome is a piece of art,");
     return (0);
 }
