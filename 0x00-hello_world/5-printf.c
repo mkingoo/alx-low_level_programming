5b23c807210bf44d6c1ba4d16ff4dcb54189938b
Michael King'oo <bmuthama.bm2@gmail.com>
2022-10-14 19:44:45 +0000

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  *My hello, printf task
  *Return: return 1 after program execution
  */
-int void(main)
+int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art, \n");
 return (0);
