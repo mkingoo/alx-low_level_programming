c78a479f49ae0043f3ec8035cdd03d50d643fcab
Michael King'oo <bmuthama.bm2@gmail.com>
2022-10-16 19:02:13 +0000

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,16 +1,21 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
  *
  * Return: Always 0 (Success/completed)
  */
+
 int main(void)
 {
 	int i, j;
+
 	i = 48;
 	j = 48;
+
 	while (i < 58)
 	{
+
 		j = i+1;
 		while (j < 58)
 		{
@@ -19,8 +24,10 @@ int main(void)
 
 			if (i < 56 || j < 57)
 			{
+
 				putchar(44);
 				putchar(32);
+
 			}
 			j++;
 		}
