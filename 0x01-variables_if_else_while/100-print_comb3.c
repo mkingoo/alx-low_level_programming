86a156d027e98a790774cc9c853bb5c70b366193
Michael King'oo <bmuthama.bm2@gmail.com>
2022-10-16 19:29:45 +0000

    comb3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -21,7 +21,7 @@ int main(void)
 		{
 			putchar(i);
 			putchar(j);
-		
+
 			if (i < 56 || j < 57)
 			{
 				putchar(44);
@@ -29,7 +29,7 @@ int main(void)
 			}
 			j++;
        		}
-
+	
 		i++;
 	}
 
