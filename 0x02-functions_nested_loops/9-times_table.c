4a48c874c836432372599872fe320b2e88e32e57
Michael King'oo <bmuthama.bm2@gmail.com>
2022-10-19 16:54:06 +0000

    message

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * times_table -prints tables
- 
+ *
  * Return: Always 0.
  */
 void times_table(void)
@@ -9,6 +9,7 @@ void times_table(void)
 	int a;
 	int b;
 	int c;
+
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
