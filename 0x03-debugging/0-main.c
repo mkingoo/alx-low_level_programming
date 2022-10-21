8308b61d2120dc369926f922e93bac6cfa778fa3
Michael King'oo <bmuthama.bm2@gmail.com>
2022-10-21 15:51:05 +0000

    trial

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * main - tests function that prints if integer is positive or negative
  *
@@ -14,5 +15,4 @@ int main(void)
 	positive_or_negative(i);
 
 	return (0);
-
 }
