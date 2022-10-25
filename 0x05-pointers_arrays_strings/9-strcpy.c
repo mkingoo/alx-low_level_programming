a83cb44c0e4c7d819635b457c64f085f573aee82
Michael King'oo <bmuthama.bm2@gmail.com>
2022-10-25 19:08:46 +0000

    message

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,25 +1,34 @@
-#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 /**
- *_strcpy - copies the string pointed to by src
- *@dest: A pointer to a char that will be changed
- *@src: A pointer to a char that will be changed
- *Return: dest
+ * main - program that generates random valid
+ * passwords for the program 101-crackme
+ * Return: Always 0 (Success)
  */
 
-char *_strcpy(char *dest, char *src)
+int main(void)
 {
+	int pass[100];
+	int i, sum, n;
 
-	int i;
+	sum = 0;
+	srand(time(NULL));
 
-	i = 0;
-
-	while (src[i] != '\0')
+	for (i = 0; i < 100; i++)
 	{
-		dest[i] = src[i];
-		i++;
+		pass[i] = rand() % 78;
+		sum += (pass[i] + '0');
+		putchar(pass[i] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			n = 2772 - sum - '0';
+			sum += n;
+			putchar(n + '0');
+			break;
+		}
 	}
-	dest[i] = '\0';
 
-	return (dest);
-
+	return (0);
 }
